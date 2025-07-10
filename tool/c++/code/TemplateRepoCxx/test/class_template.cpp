1bc81ab8d1b0f4e48ff291a3688c949a23cf5685
peng.dai <daip96286@gmail.com>
2025-07-11 00:06:21 +0800

    add c++ class template file

---
+++ tool/c++/code/TemplateRepoCxx/test/class_template.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <memory>
+#include <string>
+using namespace std;
+
+class Base 
+{
+ public:
+    Base() { cout << "created Base " << endl; }
+    ~Base() { cout << "delete Base " << endl; }
+};
+
+class Child
+{
+  public: 
+    Child() { cout << "created Child " << endl; }
+    ~Child() { cout << "delete Child " << endl; }
+};
+
+int main() {
+  cout << "*********start***********" << endl;
+  /*开始main函数处理*/
+  cout << "*********end***********" << endl;
+  return 0;
+}
+
